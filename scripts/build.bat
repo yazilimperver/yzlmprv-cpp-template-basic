d0a64eb1a823adb63ddc46bef9e49fe6b59fe41f
Yazilimperver <yazilimperver@gmail.com>
2024-06-30 21:06:53 +0300

    Yazılım oluşturma linux betiği  ve presetler güncellenmiştir.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,9 @@
+@echo off
+
+if "%1%"=="" goto Usage
+
 cmake -S . -B buildWin
 cmake --build buildWin
+
+:Usage
+echo "Expected usage: build.bat <BuildType: Debug|Release>"
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-# GCC Build
-cmake --preset debug-gcc
-cmake --build --preset debug
+Usage(){
+    echo "Expected Usage: build.sh <BuildType: Debug|Release>"
+    return 0
+}
+
+if [ "$#" -eq 0 ] ; then
+    Usage
+    exit 0
+fi
+
+BuildType=$1
+if [ "${BuildType,,}" == "debug" ] ; then
+    echo "Debug Build Selected"
+    cmake --preset debug-gcc
+    cmake --build --preset build-debug-gcc
+else
+    echo "Release Build Selected"
+    cmake --preset release-gcc
+    cmake --build --preset build-release-gcc
+fi
+
+exit 0
